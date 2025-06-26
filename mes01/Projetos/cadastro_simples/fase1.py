67694771e2e14d511aa64fc9517eb42c5fde091d
Emanuelle Machado <linakeise@gmail.com>
2025-06-25 23:33:40 -0300

    Projeto 1 – Fase 1 e README criados

---
+++ mes01/Projetos/cadastro_simples/fase1.py
@@ -0,0 +1,8 @@
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+peso = float(input("Peso (kg): "))
+
+print(f"\nCadastro realizado com sucesso!")
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Peso: {peso} kg")
