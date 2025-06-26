9ec7f0af2d40e4a388355918d45edc2ef0a7434f
Emanuelle Machado <linakeise@gmail.com>
2025-06-25 22:33:26 -0300

    Mês 1 – Aula 2: Entrada de dados e tipos

---
+++ mes01/aula02_dados_usuario.py
@@ -0,0 +1,10 @@
+nome = input("Qual é o seu nome?")
+print("Olá,", nome)
+
+nome = input("Qual é o seu nome? ")
+idade = int(input("Quantos anos você tem? "))
+peso = float(input("Qual é o seu peso? "))
+
+print("Olá,", nome + "!")
+print("Você tem", idade, "anos e pesa", peso, "kg.")
+
